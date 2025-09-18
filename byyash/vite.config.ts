c19c167519f25ffc9e16790c131bb2a355c9b730
Yash Parikh <yparikh95@gmail.com>
2025-09-18 01:10:06 -0400

    fix: deployment config

--- byyash/vite.config.ts
+++ byyash/vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/byyash/', // this tells Vite all assets are under /byyash/
-  build: {
-    outDir: 'dist',
-  },
+  base: '/',
 });
